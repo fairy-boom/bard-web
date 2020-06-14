216f2065f2d7431fdd5c8f063a27985139bfe60f
tookbra <tookbra@outlook.com>
2020-06-14 22:54:21 +0800

    :sparkles: move plugin shop component

--- src/components/Plugin/PluginShop.vue
+++ src/components/Plugin/PluginShop.vue
@@ -4,14 +4,26 @@
     <div class="plugin-shop-container card-container">
       <a-tabs default-active-key="1">
         <a-tab-pane v-for="item in pluginCategory" :key="item.id" :tab="item.name">
-          <a-carousel arrows :dots="false">
-            <div v-for="plugin in plugins" :key="plugin.id">
-              <a-card :title="plugin.name" style="width: 300px" :loading="false" :hoverable="true" :headStyle="{backgroundColor: '#1890ff'}">
+          <div class="card-carousel-wrapper">
+            <div class="card-carousel--nav__left" @click="moveCarousel(-1)" :disabled="atHeadOfList"></div>
+            <div class="plugin-card-container" :style="{ transform: 'translateX' + '(' + currentOffset + 'px' + ')'}">
+              <a-card
+                class="plugin-card"
+                v-for="plugin in plugins"
+                :key="plugin.id"
+                :title="plugin.name"
+                style="min-width: 320px"
+                :loading="false"
+                :hoverable="true"
+                :headStyle="{backgroundColor: '#F5F5F5'}">
                 <a-checkbox slot="extra"></a-checkbox>
-                <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+                <p>
+                  {{ plugin.description }}
+                </p>
               </a-card>
             </div>
-          </a-carousel>
+            <div class="card-carousel--nav__right" @click="moveCarousel(1)" :disabled="atEndOfList"></div>
+          </div>
           <a-divider />
         </a-tab-pane>
       </a-tabs>
@@ -27,15 +39,33 @@
     name: 'PluginShop',
     data () {
       return {
+        currentOffset: 0,
+        paginationFactor: 220,
+        windowSize: 7,
         pluginCategory: [],
         plugins: [],
         defaultActiveKey: 1
+      }
+    },
+    computed: {
+      atEndOfList () {
+        return this.currentOffset <= (this.paginationFactor * -1) * (this.plugins.length - this.windowSize)
+      },
+      atHeadOfList () {
+        return this.currentOffset === 0
       }
     },
     mounted () {
       this.getPluginCategory()
     },
     methods: {
+      moveCarousel (direction) {
+        if (direction === 1 && !this.atEndOfList) {
+          this.currentOffset -= this.paginationFactor
+        } else if (direction === -1 && !this.atHeadOfList) {
+          this.currentOffset += this.paginationFactor
+        }
+      },
       getPluginCategory () {
         getDictByCode('plugin').then(res => {
           if (res.success) {
@@ -58,16 +88,59 @@
     }
   }
 </script>
-<style scoped>
-  /* For demo */
-  .ant-carousel >>> .slick-slide {
-    height: 160px;
-    line-height: 160px;
-    background: #364d79;
-    overflow: hidden;
+<style lang="less">
+  .card-carousel-wrapper {
+    display: flex;
+    align-items: center;
+    margin: 20px 0 40px;
+    color: #666a73;
+
+    .card-carousel--nav__left, .card-carousel--nav__right {
+      display: inline-block;
+      width: 15px;
+      height: 15px;
+      padding: 10px;
+      box-sizing: border-box;
+      border-top: 2px solid #42b883;
+      border-right: 2px solid #42b883;
+      cursor: pointer;
+      margin: 0 10px;
+      transition: transform 150ms linear;
+    }
+
+    .card-carousel--nav__left[disabled], .card-carousel--nav__right[disabled] {
+      opacity: 0.2;
+      border-color: black;
+    }
+
+    .card-carousel--nav__left {
+      transform: rotate(-135deg);
+    }
+
+    .card-carousel--nav__left:active {
+      transform: rotate(-135deg) scale(0.9);
+    }
+    .card-carousel--nav__right {
+      transform: rotate(45deg);
+    }
+    .card-carousel--nav__right:active {
+      transform: rotate(45deg) scale(0.9);
+    }
   }
 
-  .ant-carousel >>> .slick-slide h3 {
-    color: #fff;
+  .plugin-card-container {
+    display: flex;
+    transition: transform 150ms ease-out;
+    overflow: hidden;
+
+    .plugin-card {
+      min-height: 180px;
+      cursor: pointer;
+      box-shadow: 0 4px 15px 0 rgba(40, 44, 53, 0.06), 0 2px 2px 0 rgba(40, 44, 53, 0.08);
+      background-color: #fff;
+      border-radius: 4px;
+      z-index: 3;
+      margin: 0 10px 2px;
+    }
   }
 </style>
